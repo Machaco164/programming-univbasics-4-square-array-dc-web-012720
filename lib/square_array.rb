4aa2a8c0a3d1ff8c29f705842a0e0ec684c779b8
Raul A Rozo <raul.a.rozo.o@gmail.com>
2020-01-13 21:09:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while array[counter] do
-    new_array << array[counter]**2
+    new_array[counter] << array[counter]**2
     counter += 1
   end
 end
