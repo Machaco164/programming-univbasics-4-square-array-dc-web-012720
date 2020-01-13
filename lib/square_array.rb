b154fd4f0dae1cfb8180853e9fa4c5390cffc404
Raul A Rozo <raul.a.rozo.o@gmail.com>
2020-01-13 21:15:21 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,11 @@
+
+
 def square_array(array)
   counter = 0
- 
+  new_array = []
   while array[counter] do
-    puts array[counter]**2
-    counter += 1
+    new_array << array[counter]**2
+    counter += 1 
   end
+  new_array
 end
